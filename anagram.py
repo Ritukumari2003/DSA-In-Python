1c4f04a09b4fe3465bfccfc3d1a9718c72a6b6e1
Ritukumari2003 <2503.ritu@gmail.com>
2026-03-12 14:52:39 +0530

    Anagram using dictionary

--- anagram.py
+++ anagram.py
@@ -1,6 +1,6 @@
 # Anagrams
 
-########### Using sorted ##########
+########### Using sorted ########## O(nlogn) ##########
 s1 = "listen"
 s2 = "silent"
 
@@ -11,12 +11,30 @@ for a, b in zip(sorted(s1), sorted(s2)):
         anagram = False
         break
         
+print("O(nlogn): ")
 if anagram:
-    print('Anagram')
+    print('Anagrams')
 else:
-    print('Not an anagram')
+    print('Not anagrams')
 
 # more simpler:
-print(sorted(s1) == sorted(s2))
+# print(sorted(s1) == sorted(s2))
 
-######## Using dictionary #############
+######## Using dictionary mapping ############# O(n) ##########
+mapping = [0]*26
+
+for ch in s1:
+    index = ord(ch)-97
+    mapping[index] += 1
+
+for ch in s2:
+    index = ord(ch)-97
+    mapping[index] -= 1
+
+print()
+print("O(n): ")
+if all(list(m==0 for m in mapping)):
+    print("Anagrams")
+else:
+    print("Not Anagrams")
+    
