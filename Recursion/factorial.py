3977bd5e106fbe73fd7571477c7563684802fd59
Ritukumari2003 <2503.ritu@gmail.com>
2026-03-26 16:06:15 +0530

    Recursion Basics

---
+++ Recursion/factorial.py
@@ -0,0 +1,10 @@
+# Finding factorial of a number using recursion 
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    
+    return n * factorial(n-1)
+
+n = int(input("Enter n: "))
+print("Factorial of n:", factorial(n))
---
+++ Recursion/fibonacci.py
@@ -0,0 +1,10 @@
+# Finding fibonacci of a number using recursion 
+
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    
+    return fibonacci(n-1) + fibonacci(n-2)
+
+n = int(input("Enter n: "))
+print("Fibonacci of n:", fibonacci(n))
